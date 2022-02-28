71d61513cbf284eff1bcd390e6761540c4ff9d0b
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-02-28 14:38:15 +0200

    Add files via upload

---
+++ build/precache-manifest.d51157aae01ecc7d59c46b269a4e1098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a427ad41322994493f3eb5d3fd674d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f57207fc9817460898",
+    "url": "/static/css/main.70c03fd0.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "c7f57207fc9817460898",
+    "url": "/static/js/main.4cebd389.chunk.js"
+  },
+  {
+    "revision": "a3041285a02e69056734",
+    "url": "/static/js/runtime~main.01fa5b1e.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Hello, Programmers, yess, Palinca, brother, pwp</h1>
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
